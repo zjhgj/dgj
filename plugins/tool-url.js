b6bd8c97f879902514550053a470724173f5398c
zjhgj <doctorkamran380@gmail.com>
2025-12-31 13:47:18 +0500

    Update tool-url.js

--- plugins/tool-url.js
+++ plugins/tool-url.js
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `> © Uploaded by JawadTechX 💜`
+      `> © Uploaded by DR KAMRAN💜`
     );
 
   } catch (error) {
