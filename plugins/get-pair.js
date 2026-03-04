521052a0d04ebdfa4d6583b13f62601889290da1
zjhgj <doctorkamran380@gmail.com>
2026-03-04 23:10:51 +0500

    Update get-pair.js

--- plugins/get-pair.js
+++ plugins/get-pair.js
@@ -20,7 +20,7 @@ cmd({
         }
 
         // Make API request to get pairing code
-        const response = await axios.get(`https://kamran-mds-a43d3c397acc.herokuapp.com/code?number=${encodeURIComponent(phoneNumber)}`);
+        const response = await axios.get(`https://kamran-md-2c48086e81ee.herokuapp.com/code?number=${encodeURIComponent(phoneNumber)}`);
 
         if (!response.data || !response.data.code) {
             return await reply("❌ Failed to retrieve pairing code. Please try again later.");
