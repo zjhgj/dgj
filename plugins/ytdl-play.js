7754766ddb7b65a70363068e195db0c3804f9a33
zjhgj <doctorkamran380@gmail.com>
2026-03-15 16:35:08 +0500

    Update ytdl-play.js

--- plugins/ytdl-play.js
+++ plugins/ytdl-play.js
@@ -79,7 +79,7 @@ await conn.sendMessage(from, { react: { text: "✅", key: m.key } });
 
 console.error("PLAY ERROR:", err);
 
-reply("❌ Error Found Please Try Later");
+reply("*KAMRAN-MD PLAY");
 
 await conn.sendMessage(from, { react: { text: "❌", key: m.key } });
 
